77d1ec29006495c056df0c54fa626c0bd4e00414
Lyza Fortes <lyzas2jr@gmail.com>
2024-04-15 16:19:31 -0300

    Conhecendo a linguagem

---
+++ Conhecendo a Linguagem/convertendo_tipos.py
@@ -0,0 +1,32 @@
+preco ='10.50'
+idade = '28'
+
+#convertendo de str para float
+print(float(preco))
+
+#convertendo de str para int
+print (int(idade))
+
+#convertendo float para int
+print(int (1.501552))
+
+#convertendo de str para int
+print(int ('10'))
+
+#convertendo de str para float
+print(float('10.10'))
+
+#convertendo de int para float
+print(float(100))
+
+#convertendo float para str
+print(str(10.10))
+
+#type = classe tipo da variável
+print(type(str(10.10)))
+
+# '/' retorna um valor float
+print(100 / 2)
+
+# '/' retorna um valor int
+print(100 // 2)
---
+++ Conhecendo a Linguagem/variaveis_constantes.py
@@ -0,0 +1,15 @@
+nome = 'Lyza'
+idade = '29'
+
+#Parenteses opcional
+nome, idade = 'Rafael', 34
+
+print(nome, idade)
+
+#Padrão Snack case
+limite_saque_diario = 1000
+
+#Exemplo de constante
+STATES = 'SP', 'RJ', 'SC'
+
+print(STATES)
